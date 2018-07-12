c1f5c45b830550163856a6e35c98fdb6e27cf0d4
Y08L <Y08L@nykredit.dk>
2018-07-12 16:54:39 +0200

    Lekcja MissingINteger samemu

--- Lesson4_MaxCounters/src/main/java/com/codility/Solution1.java
+++ Lesson4_MaxCounters/src/main/java/com/codility/Solution1.java
@@ -7,4 +7,22 @@ package com.codility;
  */
 
 public class Solution1 {
+    public int[] solution(int N, int[] A) {
+        int[] countersArray = new int[N];
+        int counterState = 0;
+
+        for (int value : A) {
+            if (value <= N && value >= 1 && value != N + 1) {
+                countersArray[value - 1]++;
+                counterState = countersArray[value - 1]++;
+            } else if (value == N + 1) {
+                //int counterState = countersArray[value - 1];
+                for (int i = 0; i < countersArray.length; i++) {
+                    countersArray[i] = counterState;
+                }
+            }
+        }
+
+        return countersArray;
+    }
 }
--- Lesson4_MaxCounters/src/test/java/com/codility/SolutionTest.java
+++ Lesson4_MaxCounters/src/test/java/com/codility/SolutionTest.java
@@ -1,5 +1,9 @@
 package com.codility;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Place description here.
  *
@@ -7,4 +11,20 @@ package com.codility;
  */
 
 public class SolutionTest {
+
+    @Test
+    public void test_codility() {
+        //given
+        int[] A = {3, 4, 4, 6, 1, 4, 4};
+        int N = 5;
+        int[] expectedArray = {3, 2, 2, 4, 2};
+        //when
+        int[] retult = executeSolution(N, A);
+        //then
+        assertEquals(retult, expectedArray);
+    }
+
+    private int[] executeSolution(int N, int[] A) {
+        return new Solution1().solution(N, A);
+    }
 }
